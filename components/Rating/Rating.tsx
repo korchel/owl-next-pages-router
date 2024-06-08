2badeb6488c81e05c18ed07c6da1e8d3443c96f5
Elena Korchuganova <korchel21@gmail.com>
2024-06-08 12:53:22 +0300

    rating component edited

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,23 +1,54 @@
-'use client';
 
+
+import { useState, KeyboardEvent } from 'react';
 import cn from 'classnames';
 
 import { RatingProps } from "./Rating.props";
 import styles from './Rating.module.css';
 import StarIcon from './star.svg';
-// isEditable = false, setRating, className
 
-export const Rating = ({ rating, ...props}: RatingProps): JSX.Element => {
-  // const [currentRating, setCurrentRating] = useState<number>(0);
+export const Rating = ({ rating, isEditable = false, setRating, ...props}: RatingProps): JSX.Element => {
+  const [currentRating, setCurrentRating] = useState<number>(rating);
+
+  const handleMouseMove = (number: number) => {
+    if (isEditable) {
+      setCurrentRating(number);
+    }
+  };
+
+  const handleClick = (number: number) => {
+    if (isEditable && setRating) {
+      setCurrentRating(number);
+      setRating(number);
+    }
+  };
+
+  const hadleKey = (number: number, event: KeyboardEvent<SVGElement>) => {
+    if (event.code == 'Space' && setRating) {
+      setCurrentRating(number);
+      setRating(number);
+    }
+  }
+
   return (
     <div {...props}>
       {Array.from(Array(5)).map((_, i: number) => (
-        <StarIcon
+        <span
           key={i}
-          className={cn(styles.mr5, {
-            [styles.svg]: i < rating
-          })}
-        />
+          onMouseEnter={() => handleMouseMove(i + 1)}
+          onMouseLeave={() => handleMouseMove(rating)}
+          onClick={() => handleClick(i + 1)}
+        >
+          <StarIcon
+            className={cn(styles.mr5, {
+              [styles.filled]: i < currentRating,
+              [styles.isEditable]: isEditable,
+            })}
+            tabIndex={isEditable ? 0 : -1}
+            onKeyDown={(event: KeyboardEvent<SVGElement>) => isEditable && hadleKey(i + 1, event)}
+          />
+        </span>
+        
       ))}
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import React from 'react';
-import { Button } from "../components";
+import React, { useState } from 'react';
+import { Button, Rating } from "../components";
 
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [rating, setRating] = useState<number>(4);
+  console.log(rating)
   return (
     <>
       <Button variant="primary">boom</Button>
+      <Rating rating={rating} isEditable={true} setRating={setRating} />
     </>
   );
 }
